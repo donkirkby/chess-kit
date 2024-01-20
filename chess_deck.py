36ded1eaaabda6e889adcb97e605eac2437d7b10
Don Kirkby <donkirkby@gmail.com>
2024-01-19 18:17:53 -0800

    Add pips and a grid of playing cards.

--- chess_deck.py
+++ chess_deck.py
@@ -1,10 +1,49 @@
+import typing
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
 from copy import deepcopy
+from pathlib import Path
+from subprocess import call
 
 import chess.svg
+from reportlab.lib import pagesizes
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate
 
 from diagram import Diagram
+from svg_diagram import SvgDiagram
+
+PIP_PATTERNS = """\
+---+
+   |
+   |
+   |
+---+
+   |
+ O |
+   |
+---+
+O  |
+   |
+  O|
+---+
+O  |
+ O |
+  O|
+---+
+O O|
+   |
+O O|
+---+
+O O|
+ O |
+O O|
+---+
+O O|
+O O|
+O O|
+---+
+"""
 
 
 class SvgPage:
@@ -21,6 +60,8 @@ class SvgPage:
 
 
 class SvgSymbol:
+    BASE_SIZE = 45
+
     def __init__(self, symbol: str) -> None:
         self.symbol = symbol
         self.scale = 1
@@ -37,26 +78,66 @@ class SvgSymbol:
                         f'translate({self.x} {self.y}) '
                         f'scale({self.scale}) '
                         f'rotate({self.rotation}) '
-                        f'translate(-22.5 -22.5)')
+                        f'translate({-self.BASE_SIZE/2} {-self.BASE_SIZE/2})')
         return piece_group
 
 
-class SvgCard:
-    def __init__(self, symbol: str) -> None:
-        self.symbol = symbol
+class SvgPips:
+    def __init__(self, pips: int) -> None:
+        self.pips = pips
         self.x = self.y = 0
-        self.width = 170
-        self.height = 220
+        self.scale = 1
 
     def to_element(self) -> ET.Element:
-        symbol_size = 22.5
         group = ET.Element('g')
+        group.set('transform',
+                  f'translate({self.x} {self.y}) scale({self.scale})')
+        pips = self.pips
+        pip_pattern = PIP_PATTERNS.splitlines()[pips * 4 + 1:pips * 4 + 4]
+
+        for i in range(3):
+            for j in range(3):
+                if pip_pattern[i][j] == 'O':
+                    group.append(ET.Element('circle',
+                                            {'fill': 'black',
+                                             'cx': str(10*(j-1)),
+                                             'cy': str(10*(i-1)),
+                                             'r': '3'}))
+        return group
+
+
+class SvgCard:
+    BASE_WIDTH = 171
+    BASE_HEIGHT = 266
+    PIPS = dict(p=0, n=1, b=2, r=3, q=5, k=6)
+    ONE_PIP_CODEPOINT = 0x2680
+
+    def __init__(self, symbol: str) -> None:
+        self.symbol = symbol
+        self.scale = 1
+        self.x = self.y = 0
+        self.rotation = 0
+
+    def to_element(self) -> ET.Element:
+        symbol_size = SvgSymbol.BASE_SIZE / 2
+        group = ET.Element('g')
+        group.set('transform',
+                  f'translate({self.x} {self.y}) '
+                  f'scale({self.scale}) '
+                  f'rotate({self.rotation})')
         group.append(ET.Element('rect',
-                                {'width': str(self.width),
-                                 'height': str(self.height),
-                                 'fill': '',
+                                {'width': str(self.BASE_WIDTH),
+                                 'height': str(self.BASE_HEIGHT),
+                                 'fill': 'white',
                                  'stroke': 'black',
                                  'stroke-width': '4'}))
+        pips_count = self.PIPS[self.symbol.lower()]
+        if pips_count:
+            pips = SvgPips(pips_count)
+            pips.x = self.BASE_WIDTH / 2
+            pips.y = self.BASE_HEIGHT / 2
+            pips.scale = 0.75
+            group.append(pips.to_element())
         symbol1 = SvgSymbol(self.symbol)
         symbol1.scale = 0.75
         symbol1.x = symbol_size
@@ -64,16 +145,77 @@ class SvgCard:
         group.append(symbol1.to_element())
         symbol2 = deepcopy(symbol1)
         symbol2.rotation = 180
-        symbol2.x = self.width - symbol_size
-        symbol2.y = self.height - symbol_size
+        symbol2.x = self.BASE_WIDTH - symbol_size
+        symbol2.y = self.BASE_HEIGHT - symbol_size
         group.append(symbol2.to_element())
         symbol3 = deepcopy(symbol1)
-        symbol3.scale = 2
-        symbol3.x = self.width / 2
-        symbol3.y = self.height / 2 - symbol_size*2
+        symbol3.scale = 1.75
+        symbol3.x = self.BASE_WIDTH / 2
+        symbol3.y = self.BASE_HEIGHT / 2 - symbol_size*2
         group.append(symbol3.to_element())
         symbol4 = deepcopy(symbol3)
-        symbol4.y = self.height / 2 + symbol_size*2
+        symbol4.y = self.BASE_HEIGHT / 2 + symbol_size*2
         symbol4.rotation = 180
         group.append(symbol4.to_element())
         return group
+
+
+class SvgGrid:
+    def __init__(self, symbols: typing.List[str]) -> None:
+        self.symbols = symbols
+        self.base_width = SvgCard.BASE_WIDTH * len(symbols[0])
+        self.base_height = SvgCard.BASE_HEIGHT * len(symbols)
+        self.x = self.y = 0
+        self.scale = 1
+        self.rotation = 0
+
+    def to_element(self) -> ET.Element:
+        group = ET.Element('g')
+        group.set('transform',
+                  f'translate({self.x} {self.y}) '
+                  f'scale({self.scale}) '
+                  f'rotate({self.rotation})')
+        for i, row in enumerate(self.symbols):
+            for j, symbol in enumerate(row):
+                card = SvgCard(symbol)
+                card.x = j * card.BASE_WIDTH
+                card.y = i * card.BASE_HEIGHT
+                group.append(card.to_element())
+        return group
+
+
+def main() -> None:
+    page_size = pagesizes.letter
+    vertical_margin = 0.85 * inch
+    side_margin = 0.6 * inch
+
+    pdf_path = Path(__file__).parent / 'docs' / 'chess_deck.pdf'
+    doc = SimpleDocTemplate(str(pdf_path),
+                            title='Chess Deck',
+                            author='Don Kirkby',
+                            pagesize=page_size,
+                            leftMargin=side_margin,
+                            rightMargin=side_margin,
+                            topMargin=vertical_margin,
+                            bottomMargin=vertical_margin)
+    diagrams = []
+    symbol_pages = [['rnbq', 'pppp'],
+                    ['kbnr', 'pppp'],
+                    ['PPPP', 'RNBQ'],
+                    ['PPPP', 'KBNR']]
+    for symbol_page in symbol_pages:
+        svg_page = SvgPage(7.5*inch, 9*inch)
+        grid = SvgGrid(symbol_page)
+        grid.rotation = 90
+        grid.x = 7*inch
+        grid.scale = 7*inch / grid.base_height
+        svg_page.append(grid.to_element())
+        diagram = SvgDiagram(svg_page.to_svg()).to_reportlab()
+        diagrams.append(diagram)
+    doc.build(diagrams)
+    print('Done.')
+    call(["evince", pdf_path])
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_chess_deck.py
+++ tests/test_chess_deck.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 import chess.svg
 
-from chess_deck import SvgPage, SvgSymbol, SvgCard
+from chess_deck import SvgPage, SvgSymbol, SvgCard, SvgGrid, SvgPips
 from diagram import Diagram
 from diagram_differ import DiagramDiffer
 from svg_diagram import SvgDiagram
@@ -111,14 +111,85 @@ def test_symbol_scale(diagram_differ: DiagramDiffer):
 
 
 # noinspection DuplicatedCode
-def test(diagram_differ: DiagramDiffer):
-    expected_page = SvgPage(170, 220)
+def test_pips(diagram_differ: DiagramDiffer):
+    expected_page = SvgPage(90, 90)
     expected_page.append(ET.Element('rect',
-                                    {'width': '170',
-                                     'height': '220',
-                                     'fill': '',
+                                    {'fill': 'cornsilk',
+                                     'width': '45',
+                                     'height': '45'}))
+    expected_page.append(ET.Element('circle',
+                                    {'fill': 'black',
+                                     'cx': '22.5',
+                                     'cy': '22.5',
+                                     'r': '3'}))
+    expected_diagram = SvgDiagram(expected_page.to_svg())
+
+    page = SvgPage(90, 90)
+    page.append(ET.Element('rect',
+                           {'fill': 'cornsilk',
+                            'width': '45',
+                            'height': '45'}))
+    pips = SvgPips(1)
+    pips.x = 22.5
+    pips.y = 22.5
+    page.append(pips.to_element())
+
+    svg_diagram = SvgDiagram(page.to_svg())
+    diagram_differ.assert_equal(svg_diagram, expected_diagram)
+
+
+# noinspection DuplicatedCode
+def test_pips_transform(diagram_differ: DiagramDiffer):
+    expected_page = SvgPage(190, 190)
+    expected_page.append(ET.Element('rect',
+                                    {'fill': 'cornsilk',
+                                     'x': '100',
+                                     'y': '100',
+                                     'width': '90',
+                                     'height': '90'}))
+    expected_page.append(ET.Element('circle',
+                                    {'fill': 'black',
+                                     'cx': '125',
+                                     'cy': '125',
+                                     'r': '6'}))
+    expected_page.append(ET.Element('circle',
+                                    {'fill': 'black',
+                                     'cx': '165',
+                                     'cy': '165',
+                                     'r': '6'}))
+    expected_diagram = SvgDiagram(expected_page.to_svg())
+
+    page = SvgPage(190, 190)
+    page.append(ET.Element('rect',
+                           {'fill': 'cornsilk',
+                            'x': '100',
+                            'y': '100',
+                            'width': '90',
+                            'height': '90'}))
+    pips = SvgPips(2)
+    pips.x = 145
+    pips.y = 145
+    pips.scale = 2
+    page.append(pips.to_element())
+
+    svg_diagram = SvgDiagram(page.to_svg())
+    diagram_differ.assert_equal(svg_diagram, expected_diagram)
+
+
+# noinspection DuplicatedCode
+def test_card(diagram_differ: DiagramDiffer):
+    expected_page = SvgPage(171, 266)
+    expected_page.append(ET.Element('rect',
+                                    {'width': '171',
+                                     'height': '266',
+                                     'fill': 'white',
                                      'stroke': 'black',
                                      'stroke-width': '4'}))
+    expected_pips = SvgPips(5)
+    expected_pips.x = 85.5
+    expected_pips.y = 133
+    expected_pips.scale = 0.75
+    expected_page.append(expected_pips.to_element())
     card_symbol = 'Q'
     expected_piece = SvgSymbol(card_symbol)
     expected_piece.scale = 0.75
@@ -127,23 +198,132 @@ def test(diagram_differ: DiagramDiffer):
     expected_page.append(expected_piece.to_element())
     expected_piece2 = deepcopy(expected_piece)
     expected_piece2.rotation = 180
-    expected_piece2.x = 170 - expected_piece.x
-    expected_piece2.y = 220 - expected_piece.y
+    expected_piece2.x = 171 - expected_piece.x
+    expected_piece2.y = 266 - expected_piece.y
     expected_page.append(expected_piece2.to_element())
     expected_piece3 = deepcopy(expected_piece)
-    expected_piece3.x = 85
-    expected_piece3.y = 110 - 45
-    expected_piece3.scale = 2
+    expected_piece3.x = 85.5
+    expected_piece3.y = 133 - 45
+    expected_piece3.scale = 1.75
     expected_page.append(expected_piece3.to_element())
     expected_piece4 = deepcopy(expected_piece3)
     expected_piece4.rotation = 180
-    expected_piece4.y = 110 + 45
+    expected_piece4.y = 133 + 45
     expected_page.append(expected_piece4.to_element())
     expected_diagram = SvgDiagram(expected_page.to_svg())
 
-    page = SvgPage(170, 220)
+    page = SvgPage(171, 266)
     card = SvgCard(card_symbol)
     page.append(card.to_element())
+    svg_diagram = SvgDiagram(page.to_svg())
+
+    diagram_differ.assert_equal(svg_diagram, expected_diagram)
+
+
+# noinspection DuplicatedCode
+def test_card_transform(diagram_differ: DiagramDiffer):
+    expected_page = SvgPage(171, 266)
+    expected_card1 = SvgCard('Q').to_element()
+    expected_card1.set('transform', 'translate(133 0) scale(0.5) rotate(90)')
+    expected_page.append(expected_card1)
+    expected_card2 = SvgCard('N').to_element()
+    expected_card2.set('transform', 'translate(85.5 133) scale(0.5)')
+    expected_page.append(expected_card2)
+    expected_diagram = SvgDiagram(expected_page.to_svg())
+
+    page = SvgPage(171, 266)
+    card1 = SvgCard('Q')
+    card1.scale = 0.5
+    card1.rotation = 90
+    card1.x = 133
+    page.append(card1.to_element())
+    card2 = SvgCard('N')
+    card2.scale = 0.5
+    card2.x = 85.5
+    card2.y = 133
+    page.append(card2.to_element())
+    svg_diagram = SvgDiagram(page.to_svg())
+
+    diagram_differ.assert_equal(svg_diagram, expected_diagram)
+
+
+# noinspection DuplicatedCode
+def test_grid(diagram_differ: DiagramDiffer):
+    expected_page = SvgPage(260, 266)
+    expected_page.append(ET.Element('rect',
+                                    {'width': '260',
+                                     'height': '266',
+                                     'fill': 'ivory'}))
+    expected_card1 = SvgCard('P')
+    expected_card1.scale = 0.5
+    expected_page.append(expected_card1.to_element())
+    expected_card2 = SvgCard('N')
+    expected_card2.scale = 0.5
+    expected_card2.x = 85.5
+    expected_page.append(expected_card2.to_element())
+    expected_card3 = SvgCard('B')
+    expected_card3.scale = 0.5
+    expected_card3.x = 171
+    expected_page.append(expected_card3.to_element())
+    expected_card4 = SvgCard('R')
+    expected_card4.scale = 0.5
+    expected_card4.y = 133
+    expected_page.append(expected_card4.to_element())
+    expected_card5 = SvgCard('Q')
+    expected_card5.scale = 0.5
+    expected_card5.x = 85.5
+    expected_card5.y = 133
+    expected_page.append(expected_card5.to_element())
+    expected_card6 = SvgCard('K')
+    expected_card6.scale = 0.5
+    expected_card6.x = 171
+    expected_card6.y = 133
+    expected_page.append(expected_card6.to_element())
+    expected_diagram = SvgDiagram(expected_page.to_svg())
+
+    page = SvgPage(260, 266)
+    page.append(ET.Element('rect',
+                           {'width': '260',
+                            'height': '266',
+                            'fill': 'ivory'}))
+    grid = SvgGrid(['PNB', 'RQK'])
+    grid.scale = 266 / grid.base_height
+    page.append(grid.to_element())
+    svg_diagram = SvgDiagram(page.to_svg())
+
+    diagram_differ.assert_equal(svg_diagram, expected_diagram)
+
+
+# noinspection DuplicatedCode
+def test_grid_transform(diagram_differ: DiagramDiffer):
+    expected_page = SvgPage(133, 171)
+    expected_page.append(ET.Element('rect',
+                                    {'width': '260',
+                                     'height': '266',
+                                     'fill': 'ivory'}))
+    expected_card1 = SvgCard('P')
+    expected_card1.scale = 0.5
+    expected_card1.rotation = 90
+    expected_card1.x = 133
+    expected_page.append(expected_card1.to_element())
+    expected_card2 = SvgCard('N')
+    expected_card2.scale = 0.5
+    expected_card2.rotation = 90
+    expected_card2.x = 133
+    expected_card2.y = 85.5
+    expected_page.append(expected_card2.to_element())
+    expected_diagram = SvgDiagram(expected_page.to_svg())
+
+    page = SvgPage(133, 171)
+    page.append(ET.Element('rect',
+                           {'width': '260',
+                            'height': '266',
+                            'fill': 'ivory'}))
+    grid = SvgGrid(['PN'])
+    grid.rotation = 90
+    grid.x = 133
+    grid.scale = 171 / grid.base_width
+    page.append(grid.to_element())
     svg_diagram = SvgDiagram(page.to_svg())
 
     diagram_differ.assert_equal(svg_diagram, expected_diagram)
